cf414f4723877c269a3232e7d1c7c813ca4e56df
Michael Sefranek <msefran@gmail.com>
2017-02-16 08:07:08 -0600

    this morning

--- Waldo/public/scripts.js
+++ Waldo/public/scripts.js
@@ -2,21 +2,12 @@ window.addEventListener("load",function(){
 	var waldoPic = document.getElementsByTagName("html");
 	var dimmer = document.getElementsByClassName("overlay");
 
-	const WALDOXY = [
-						{"xmin": 530,
-						"xmax": 557,
-						"ymin": 358,
-						"ymax": 400} 
-					];
-
 	//function to log the coordinates of a click
 	waldoPic[0].addEventListener("click", isWaldo, false);
 
 	function getClickPosition(e){
 		var xPos = e.clientX;
 		var yPos = e.clientY;
-		// console.log(xPos);
-		// console.log(yPos);
 		return [
 			{"x": xPos},
 			{"y": yPos}
@@ -44,15 +35,6 @@ window.addEventListener("load",function(){
 		request.open("POST","/?waldoX="+ xCor.toString() +"&waldoY=" + yCor.toString() );
 		request.addEventListener("load", display);
 		request.send();
-
-
-		//	if true display: block
-		// otherwise nothing
-		
 	}
-
-	
-
-
 
 });
